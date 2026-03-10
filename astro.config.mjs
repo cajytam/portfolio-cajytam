486ef5c8a8700d320b911bfa6c27bb25625f4cab
Cédric Matyja <c.matyja@gmail.com>
2026-03-10 22:18:38 +0100

    fix: warning `no adapter`

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,6 @@ const isDevMode = import.meta.env.DEV;
 // https://astro.build/config
 export default defineConfig({
   site: "https://cajytam.fr",
-  output: "static",
   integrations: [
     icon({
       include: {
