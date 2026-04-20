2966f59982c011410504ac837089d0e0d34a948b
Cédric Matyja <c.matyja@gmail.com>
2026-04-20 22:42:15 +0200

    build(config:nuxt): ajout `ssr: true`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,19 +4,16 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
+  ssr: true,
   experimental: {
     serverAppConfig: false,
   },
-
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ['@vue/devtools-core', '@vue/devtools-kit'],
     },
   },
-
   modules: ['@nuxt/content', '@vueuse/motion/nuxt'],
-
   css: ['~/assets/css/main.css'],
 })
