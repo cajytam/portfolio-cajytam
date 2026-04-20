36318549b695d1b62f423b4f361020e84df9a8eb
Cédric Matyja <c.matyja@gmail.com>
2026-04-20 23:34:10 +0200

    feat: ajout de la configuration de langue pour l'application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
+  app: {
+    head: {
+      htmlAttrs: { lang: 'fr' },
+    },
+  },
   experimental: {
     serverAppConfig: false,
   },
